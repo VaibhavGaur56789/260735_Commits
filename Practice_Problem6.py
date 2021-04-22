19971cbbf3a0775ea2db48ec2f28d6d4a7c713bc
Vaibhav Gaur <vaibhavgaur56789@gmail.com>
2021-04-22 22:49:17 +0530

    Input abbreviations.

---
+++ Practice_Problem6.py
@@ -0,0 +1,17 @@
+# Check for user inInput abbreviation
+
+s1 = input("Enter a string")
+#s2 = input("Enter a string")
+#s3 = input("Enter a string")
+#s4 = input("Enter a string")
+
+if s1 == "lol":
+    print("Laughing out loud")
+elif s1 == "rofl":
+    print("rolling on the floor laughing")
+elif s1 == "lmk":
+    print("let me know")
+elif s1 == "smh":
+    print("shaking my head")
+else:
+    print("No keyword")
